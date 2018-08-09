5f7307f086946318215c131842702584b003754c
jack <jack@163.com>
2018-08-09 20:40:05 +0800

    第一次提交

---
+++ src/views/login.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="login-wrap">
+        <el-form-item label="活动区域">
+            <el-select v-model="form.region" placeholder="请选择活动区域">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+            </el-select>
+        </el-form-item>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
